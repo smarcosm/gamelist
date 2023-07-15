f10a0da09274f8fe3d8e1244c14b5961ffe053cf
smarcosm <sebastiaomarcos49@gmail.com>
2023-07-15 10:19:04 -0300

    feat: :sparkles: Game seed

--- backend/gamelist/gamelist/src/main/java/com/smarcosm/gamelist/entities/Game.java
+++ backend/gamelist/gamelist/src/main/java/com/smarcosm/gamelist/entities/Game.java
@@ -7,16 +7,20 @@ import java.util.Objects;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @Column(name = "game_year")
     private  Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game (){}
