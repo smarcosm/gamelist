793a7942a95bd7cd868df2f8dd3603d9029e0979
smarcosm <sebastiaomarcos49@gmail.com>
2023-07-15 22:37:54 -0300

    build: :rocket: Homolog ok - 1

--- src/main/java/com/smarcosm/gamelist/projections/GameMinProjection.java
+++ src/main/java/com/smarcosm/gamelist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.smarcosm.gamelist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
